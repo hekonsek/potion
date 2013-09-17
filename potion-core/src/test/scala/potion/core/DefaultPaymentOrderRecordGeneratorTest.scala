01a04aa1d9bff17cef7c1ab211b9e37c65362c69
Henryk Konsek <hekonsek@gmail.com>
2013-09-17 12:33:45 +0200

    Reformatting.

--- potion-core/src/test/scala/potion/core/DefaultPaymentOrderRecordGeneratorTest.scala
+++ potion-core/src/test/scala/potion/core/DefaultPaymentOrderRecordGeneratorTest.scala
@@ -15,14 +15,18 @@ class DefaultPaymentOrderRecordGeneratorTest extends FunSuite {
 
   test("Should generate example from ING specification.") {
     val ingReferenceExample = fromInputStream(getClass.getResourceAsStream("ing_elixir_example.txt")).getLines().next()
-    val ingReferenceDate = paymentOrderDateFormat.parse("20040510")
     val paymentOrder = PaymentOrder(
-      transactionType = 110, dateOfPayment = ingReferenceDate, amount = 403595, senderBankSettlementNumber = 10501038,
-      senderBankAccountNumber = "29105010381000002201994791", receiverBankAccountNumber = "40109018700000000100198454",
+      transactionType = 110,
+      dateOfPayment = paymentOrderDateFormat.parse("20040510"),
+      amount = 403595,
+      senderBankSettlementNumber = 10501038,
+      senderBankAccountNumber = "29105010381000002201994791",
+      receiverBankAccountNumber = "40109018700000000100198454",
       senderNameAndAddress = Seq("NAZWA_STRONY_ZLECAJACEJ", "RESZTA_NAZWY", "ULICA_STRONY_ZLECAJACEJ", "MIASTO_STRONY_ZLECAJACEJ"),
       receiverNameAndAddress = Seq("NAZWA_KONTRAHENTA", "RESZTA_NAZWY", "UL._KONTRAHENTA", "MIASTO_KONTRAHENTA"),
       receiverBankSettlementNumber = 10901870,
-      descriptionOfPayment = Seq("OPIS_PLATNOSCI_1", "OPIS_PLATNOSCI_2", "", ""), clientCorrelationId = Some("CLIENTID")
+      descriptionOfPayment = Seq("OPIS_PLATNOSCI_1", "OPIS_PLATNOSCI_2", "", ""),
+      clientCorrelationId = Some("CLIENTID")
     )
 
     expectResult(ingReferenceExample) {
