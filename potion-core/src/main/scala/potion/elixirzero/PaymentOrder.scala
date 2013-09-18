f8a62d89ce74d428ac5cc7356144965b8a5dfaea
Henryk Konsek <hekonsek@gmail.com>
2013-09-18 17:09:11 +0200

    Refactoring.

--- potion-core/src/main/scala/potion/elixirzero/PaymentOrder.scala
+++ potion-core/src/main/scala/potion/elixirzero/PaymentOrder.scala
@@ -2,8 +2,15 @@ package potion.elixirzero
 
 import java.util.Date
 
-case class PaymentOrder(transactionType: Int, dateOfPayment: Date, amount: Long, senderBankSettlementNumber: Long,
-                        senderBankAccountNumber: String, receiverBankAccountNumber: String,
-                        senderNameAndAddress: Seq[String], receiverNameAndAddress: Seq[String],
-                        receiverBankSettlementNumber: Long, descriptionOfPayment: Seq[String],
-                        clientCorrelationId: Option[String], sorbnet: Boolean = false)
+case class PaymentOrder(transactionType: Int,
+                        dateOfPayment: Date,
+                        amount: Long,
+                        senderBankSettlementNumber: Long,
+                        senderBankAccountNumber: String,
+                        receiverBankAccountNumber: String,
+                        senderNameAndAddress: Seq[String],
+                        receiverNameAndAddress: Seq[String],
+                        receiverBankSettlementNumber: Long,
+                        descriptionOfPayment: Seq[String],
+                        clientCorrelationId: Option[String],
+                        sorbnet: Boolean = false)
